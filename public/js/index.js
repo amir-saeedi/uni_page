0444d62d524dbbcf4f6bbcb371afae44f09feddd
amir-saeedi <saeediamir1381@gmail.com>
2022-01-31 17:03:18 +0330

    animated home page

--- public/js/index.js
+++ public/js/index.js
@@ -21,3 +21,31 @@ $(".owl-carousel").owlCarousel({
     },
   },
 });
+//
+const newsLfet = document.querySelector(".news_lfet");
+const newsUp = document.querySelector(".news_up");
+const newsRight = document.querySelector(".news_right");
+
+const HSectionVideo = document.querySelector("#H_section-2");
+const HSectiontext = document.querySelector("#H_section-3");
+
+const sectionAnimat = function (name, cl) {
+  const revealSection = function (entries, observer) {
+    const [entry] = entries;
+    if (entry.isIntersecting) {
+      entry.target.style.opacity = 1;
+      entry.target.classList.add(cl);
+      observer.unobserve(entry.target);
+    }
+  };
+  const sectionObserver = new IntersectionObserver(revealSection, {
+    root: null,
+    threshold: 0.15,
+  });
+  sectionObserver.observe(name);
+};
+sectionAnimat(newsLfet, "animate__backInLeft");
+sectionAnimat(newsUp, "animate__backInUp");
+sectionAnimat(newsRight, "animate__backInRight");
+sectionAnimat(HSectionVideo, "animate__fadeInUp");
+sectionAnimat(HSectiontext, "animate__lightSpeedInRight");
