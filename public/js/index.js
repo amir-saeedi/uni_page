55ce71f95e3adea647d4cc8469f85606f57d6f54
amir-saeedi <saeediamir1381@gmail.com>
2022-02-02 20:53:49 +0330

    animated course page

--- public/js/index.js
+++ public/js/index.js
@@ -21,14 +21,7 @@ $(".owl-carousel").owlCarousel({
     },
   },
 });
-//
-const newsLfet = document.querySelector(".news_lfet");
-const newsUp = document.querySelector(".news_up");
-const newsRight = document.querySelector(".news_right");
-
-const HSectionVideo = document.querySelector("#H_section-2");
-const HSectiontext = document.querySelector("#H_section-3");
-
+//  animate
 const sectionAnimat = function (name, cl) {
   const revealSection = function (entries, observer) {
     const [entry] = entries;
@@ -44,8 +37,30 @@ const sectionAnimat = function (name, cl) {
   });
   sectionObserver.observe(name);
 };
-sectionAnimat(newsLfet, "animate__backInLeft");
-sectionAnimat(newsUp, "animate__backInUp");
-sectionAnimat(newsRight, "animate__backInRight");
-sectionAnimat(HSectionVideo, "animate__fadeInUp");
-sectionAnimat(HSectiontext, "animate__lightSpeedInRight");
+if (document.querySelector("#H_header")) {
+  // home page animated
+  const newsLfet = document.querySelector(".news_lfet");
+  const newsUp = document.querySelector(".news_up");
+  const newsRight = document.querySelector(".news_right");
+
+  const HSectionVideo = document.querySelector("#H_section-2");
+  const HSectiontext = document.querySelector("#H_section-3");
+
+  sectionAnimat(newsLfet, "animate__backInLeft");
+  sectionAnimat(newsUp, "animate__backInUp");
+  sectionAnimat(newsRight, "animate__backInRight");
+  sectionAnimat(HSectionVideo, "animate__fadeInUp");
+  sectionAnimat(HSectiontext, "animate__lightSpeedInRight");
+} else if (document.querySelector("#cours_header")) {
+  // courses page animated
+  const courseleft = document.querySelector(".course_left");
+  const courseUp1 = document.querySelector(".course_up1");
+  const courseUp2 = document.querySelector(".course_up2");
+  const courseRight = document.querySelector(".course_right");
+
+  sectionAnimat(courseleft, "animate__backInLeft");
+  sectionAnimat(courseUp1, "animate__backInUp");
+  sectionAnimat(courseUp2, "animate__backInUp");
+  sectionAnimat(courseRight, "animate__backInRight");
+}
+//
