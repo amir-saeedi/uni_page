c2fb68c28bf3ab2f1a26b538c8c66536469a69e5
amir-saeedi <saeediamir1381@gmail.com>
2022-06-06 17:26:23 +0430

    change style book

--- public/js/main/technicJs.js
+++ public/js/main/technicJs.js
@@ -14,4 +14,31 @@ export default function () {
   const technicList = document.getElementById("technic-List");
   technicList.innerHTML = "";
   TechniHTML.map((e) => technicList.insertAdjacentHTML("afterbegin", e));
+  //
+  const secLeft = document.querySelector(".sec-left");
+  let checkerboard = document.getElementById("board");
+  checkerboard.innerHTML = "";
+  const createBoard = function () {
+    for (let i = 0; i < 49; i++) {
+      checkerboard.insertAdjacentHTML("afterbegin", "<div></div>");
+    }
+  };
+  createBoard();
+  const para = document.createElement("div");
+  para.classList.add("checkerboard");
+  para.id = "board";
+  const src = [
+    "/public/img/technic/technic/1.jpg",
+    "/public/img/technic/technic/2.jpg",
+    "/public/img/technic/technic/3.jpg",
+  ];
+  let numberURL = 1;
+  setInterval(() => {
+    checkerboard.remove();
+    numberURL === 5 ? (numberURL = 1) : numberURL++;
+    para.style.backgroundImage = `url(/public/img/technic/technic/${numberURL}.jpg)`;
+    secLeft.appendChild(para);
+    checkerboard = document.getElementById("board");
+    createBoard();
+  }, 8000);
 }
